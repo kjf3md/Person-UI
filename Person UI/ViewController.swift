a87c985698c71205ae093b0928bee057640a2bdf
Kevin Fritz <kevinfritz@MacBook-Pro.local>
2017-09-17 18:40:30 -0500

    Initial Commit

---
+++ Person UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Kevin Fritz on 9/17/17.
+//  Copyright © 2017 Kevin Fritz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
